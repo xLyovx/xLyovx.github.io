1a6bd4169758fe1c7bb0004d50dea6f230d1d457
Jesús Carreño <jesuscarrenooliveri97@gmail.com>
2023-10-24 09:35:40 -0400

    actualizacion y mejora

--- jquery/index.js
+++ jquery/index.js
@@ -8,6 +8,20 @@ window.addEventListener("scroll", function () {
     navbar.style.backgroundColor = "transparent";
   }
 });
+const imagenClickeable = document.getElementById("ca");
+const menu = document.getElementById("cmn");
+let menuVisible = false; 
+
+imagenClickeable.addEventListener("click", () => {
+  if (!menuVisible) {
+    menu.style.display = "block";
+    menuVisible = true; 
+  } else {
+    menu.style.display = "none"; 
+    menuVisible = false; 
+  }
+});
+
 
 function scrollToSection1(sectionId) {
   let section = document.getElementById(sectionId);
@@ -39,3 +53,33 @@ function scrollToSection5(sectionId) {
     window.scroll({ top: 1250, behavior: "smooth" });
   }
 }
+
+var form = document.getElementById("my-form");  
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+		alert("Hemos recibido tu comentario. ¡Gracias por tomarte el tiempo para escribirlo!");
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oh, ocurrió un error al procesar tu formulario"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oh, ocurrió un error al procesar tu formulario"
+      });
+    }
+    form.addEventListener("submit", handleSubmit);
